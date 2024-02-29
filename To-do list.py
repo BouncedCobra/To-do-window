884383f76d7c2e493e864799264774bd40f9e204
Sergio Trujillo <sergio.trujillo@mxmart.mx>
2024-02-29 17:30:02 -0600

    Modificacion del codigo

--- To-do list.py
+++ To-do list.py
@@ -2,7 +2,7 @@ from tkinter import *
 import tkinter.messagebox
 
 def entertask():
-    #A new window to pop up to take input
+    #Nueva ventana para escribir una tarea
     input_text=""
     def add():
         input_text=entry_task.get(1.0, "end-1c")
@@ -10,7 +10,7 @@ def entertask():
             tkinter.messagebox.showwarning(title="Warning!",message="Ingresa algun texto")
         else:
             listbox_task.insert(END,input_text)
-            #close the root1 window
+            #Cerramos la ventana nueva
             root1.destroy()
     root1=Tk()
     root1.title("Agregar tarea")
@@ -20,20 +20,21 @@ def entertask():
     button_temp.pack()
     root1.mainloop()
     
-#function to facilitate the delete task from the Listbox
+#Funcion para eliminar tareas
 def deletetask():
-    #selects the selected item and then deletes it 
+    #Obtenemos el elemento seleccionado y lo eliminamos
     selected=listbox_task.curselection()
     listbox_task.delete(selected[0])
-#Executes this to mark completed 
+    
+#Funcion para marcar como completada la tarea
 def markcompleted():
     marked=listbox_task.curselection()
     temp=marked[0]
-    #store the text of selected item in a string
+    #Convierte el texto seleccionado en un String
     temp_marked=listbox_task.get(marked)
-    #update it 
+    #Se agrega un ✔ 
     temp_marked=temp_marked+" ✔"
-    #delete it then insert it 
+    #Elimina el anterior y se agrega el nuevo
     listbox_task.delete(temp)
     listbox_task.insert(temp,temp_marked)
     
